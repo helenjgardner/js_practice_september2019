b636e973309c2f74c76969e312125aad124bf7c6
helenjgardner <helen_gardner@hotmail.com>
2019-10-15 14:46:03 +0100

    Implemented challenges using .forEach

--- challenges/week4.js
+++ challenges/week4.js
@@ -1,11 +1,15 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
   let newArray = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] < 1) {
-      newArray.push(nums[i]);
-    }
-  }
+  // for (let i = 0; i < nums.length; i++) {
+  //   if (nums[i] < 1) {
+  //     newArray.push(nums[i]);
+  //   }
+  // }
+  nums.forEach(function (item) {
+    if (item < 1) { newArray.push(item) }
+  })
+
   return newArray;
 }
 
@@ -13,54 +17,76 @@ function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
   let newArray = [];
-  for (let i = 0; i < names.length; i++) {
-    if (names[i].slice(0, 1) === char) {
-      newArray.push(names[i]);
+  // for (let i = 0; i < names.length; i++) {
+  //   if (names[i].slice(0, 1) === char) {
+  //     newArray.push(names[i]);
+  //   }
+  // }
+  names.forEach(function (item) {
+    if (item.slice(0, 1) === char) {
+      newArray.push(item);
     }
-  }
+  })
   return newArray;
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
   let newArray = [];
-  for (let i = 0; i < words.length; i++) {
-    if (words[i].slice(0, 3) === 'to ') {
-      newArray.push(words[i]);
+  // for (let i = 0; i < words.length; i++) {
+  //   if (words[i].slice(0, 3) === 'to ') {
+  //     newArray.push(words[i]);
+  //   }
+  // }
+  words.forEach(function (item) {
+    if (item.slice(0, 3) === 'to ') {
+      newArray.push(item);
     }
-  }
+  })
   return newArray;
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
   let newArray = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (Number.isInteger(nums[i])) {
-      newArray.push(nums[i]);
+  // for (let i = 0; i < nums.length; i++) {
+  //   if (Number.isInteger(nums[i])) {
+  //     newArray.push(nums[i]);
+  //   }
+  // }
+  nums.forEach(function (item) {
+    if (Number.isInteger(item)) {
+      newArray.push(item);
     }
-  }
+  })
   return newArray;
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
   let newArray = [];
-  for (let i = 0; i < users.length; i++) {
-    newArray.push((users[i].data.city.displayName));
-  }
+  // for (let i = 0; i < users.length; i++) {
+  //   newArray.push((users[i].data.city.displayName));
+  // }
+  users.forEach((n) => {
+    newArray.push(n.data.city.displayName);
+  })
   return newArray;
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
   let newArray = [];
-  for (let i = 0; i < nums.length; i++) {
-
-    let result = Math.sqrt(nums[i]);
+  // for (let i = 0; i < nums.length; i++) {
+  //   let result = Math.sqrt(nums[i]);
+  //   let resultDP = result.toFixed(2);
+  //   newArray.push(Number(resultDP));
+  // }(
+  nums.forEach((item) => {
+    let result = Math.sqrt(item);
     let resultDP = result.toFixed(2);
     newArray.push(Number(resultDP));
-  }
+  })
   return newArray;
 }
 
@@ -68,21 +94,30 @@ function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
   let newArray = [];
-  for (let i = 0; i < sentences.length; i++) {
-    if (sentences[i].toLowerCase().indexOf(str.toLowerCase()) > -1) {
-      newArray.push(sentences[i]);
+  // for (let i = 0; i < sentences.length; i++) {
+  //   if (sentences[i].toLowerCase().indexOf(str.toLowerCase()) > -1) {
+  //     newArray.push(sentences[i]);
+  //   }
+  // }
+  sentences.forEach(function (n) {
+    if (n.toLowerCase().indexOf(str.toLowerCase()) > -1) {
+      newArray.push(n);
     }
-  }
+  })
   return newArray;
 }
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
   let newArray = [];
-  for (let i = 0; i < triangles.length; i++) {
-    let max = Math.max(...triangles[i]);
+  // for (let i = 0; i < triangles.length; i++) {
+  //   let max = Math.max(...triangles[i]);
+  //   newArray.push(max);
+  // }
+  triangles.forEach(function (item) {
+    let max = Math.max(...item);
     newArray.push(max);
-  }
+  })
   return newArray;
 }
 
